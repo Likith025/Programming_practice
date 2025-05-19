41a5c0bb7b42c4b6eacff5b05e8196a7ea7f75c5
likith <likithrgowda8@gmail.com>
2025-05-19 19:56:04 +0530

    included git ignore to c_prac and implemented char_freq(still improvement needed)

--- c_prac/char_freq.c
+++ c_prac/char_freq.c
@@ -3,27 +3,24 @@
 #include<string.h>
 int main(){
     char str[100];
+    int i=0;
+    int count=1;
     printf("enter a string\n");
     fgets(str,sizeof(str),stdin);
-    int i=0,count=0;
-    // while(str[i]!='\n'){
-    //     printf("%c\t",str[i]);
-        
-    //     i++;
-    // }
-    for(int i=0;i<strlen(str);i++){
-       if(str[i]=='\0'){
+    printf("%s",str);
+   for(int i=0;i<strlen(str);i++){
         for(int j=0;j<strlen(str);j++){
-            if(str[i]==str[j]){
-                count++;
-                if(i!=j){
-                    str[j]='\0';}
+            if(j!=i){
+                if(str[j]=='\0')
+                    continue;
+                if(str[i]==str[j]){
+                    count++;
+                    //str[j]='\0';
                 }
-
+            }
         }
-    }
-        printf("%c appered %d times\n",str[i],count);
-        count=0;
-    }
+        printf("%c appeared %d times \n",str[i],count);
+        count=1;
+   }
 
 }
