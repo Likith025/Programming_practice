d3a5d4324182aa268e53d578a968dcbc3b321cbf
likith <likithrgowda8@gmail.com>
2025-06-18 20:44:37 +0530

    included bin_dec

--- c_prac/dec_bin.c
+++ c_prac/dec_bin.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<string.h>
+#include<math.h>
 void dec_bin(int num){
     int bin[32];
     int started=0;
@@ -17,6 +19,19 @@ void dec_bin(int num){
 }
 
 
+void bin_dec(char*bin){
+    int dec=0;
+    int len=strlen(bin);
+    for(int i=0;i<=len;i++){
+        if(bin[i]=='1'){
+            dec += (1 << (len - i - 1));
+        }
+    }
+    printf("%d \n",dec);
+
+}
+
+
 int main(){
     int num;
     printf("enter the number\n");
@@ -26,5 +41,6 @@ int main(){
 
     }
     dec_bin(num);
+    bin_dec("11101");
     return 0;
 }
